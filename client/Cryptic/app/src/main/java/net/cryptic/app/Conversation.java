e6b4e71171904f04cc5a52e6dfdc20d70a0c3b4f
Insomnawolf <insomnawolf@gmail.com>
2017-04-20 04:01:12 -0500

    Created the conversations view!

---
+++ client/Cryptic/app/src/main/java/net/cryptic/app/Conversation.java
@@ -0,0 +1,47 @@
+package net.cryptic.app;
+
+import java.util.Date;
+
+/**
+ * Created by Edward on 4/20/2017.
+ */
+
+public class Conversation {
+    private String fromUser;
+    private Date date;
+
+    public Conversation(String fromUser, Date date)
+    {
+        this.fromUser = fromUser + "\n Last Message ";
+        this.date = date;
+        long diff = Math.abs(date.getTime() - (new Date()).getTime());
+        int diffSec = (int)(diff/1000);
+        if(diffSec < 60)
+            this.fromUser += "seconds ago";
+        else if((diffSec > 60) && (diffSec < (60 * 60)))
+            this.fromUser += "over a minute ago";
+        else if((diffSec > (60 * 60)) && (diffSec < (60 * 60 * 24)))
+            this.fromUser += "over an hour ago";
+        else if((diffSec > (60 * 60 * 24)))
+            this.fromUser += "over a day ago";
+        else
+            this.fromUser += "long long ago...";
+
+    }
+
+    public String getFromUser() {
+        return fromUser;
+    }
+
+    public void setFromUser(String fromUser) {
+        this.fromUser = fromUser;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
--- client/Cryptic/app/src/main/java/net/cryptic/app/ScrollingActivity.java
+++ client/Cryptic/app/src/main/java/net/cryptic/app/ScrollingActivity.java
@@ -1,13 +1,33 @@
 package net.cryptic.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static net.cryptic.app.R.layout.activity_scrolling;
 
 public class ScrollingActivity extends AppCompatActivity {
+
+    List<Conversation> conversations = new ArrayList<>();
+    ListView messages;
+    ConversationAdapter adapter;
+    List<String> stringList = new ArrayList<>();
+    public static final ScrollingActivity scrollingActivity = new ScrollingActivity();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,14 +35,41 @@ public class ScrollingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_scrolling);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button signOut = (Button) findViewById(R.id.signOut);
+        signOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //Log.d("Debug", "SIGNED OUT! Technically...not really.");
+
+                startActivity(new Intent(ScrollingActivity.this, LoginActivity.class));
             }
         });
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+        Date date2 = new Date();
+        Date date3 = new Date();
+        try {
+            date2 = format.parse("2017/04/20 12:24:34");
+            date3 = format.parse("2016/04/19 12:24:34");
+        } catch (ParseException e){
+            e.printStackTrace();
+        }
+        conversations.add(new Conversation("Andrew", date));
+        conversations.add(new Conversation("Edward", date2));
+        conversations.add(new Conversation("Sean", date3));
+
+        for(Conversation c : conversations)
+        {
+            stringList.add(c.getFromUser());
+            Log.d("OUTPUT", c.getFromUser());
+            Log.d("OUTPUT", c.getDate().toString());
+        }
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,
+                R.layout.listview, stringList);
+
+        messages = (ListView) findViewById(R.id.convList);
+        messages.setAdapter(adapter);
     }
 }
