f8981ed741343d53bd3e37087d976f4cae6e3793
sphassan <sphassan@sbcglobal.net>
2017-05-02 17:45:56 -0500

    Generalized randomness provider

--- client/Cryptic/app/src/main/java/net/cryptic/app/ChatActivity.java
+++ client/Cryptic/app/src/main/java/net/cryptic/app/ChatActivity.java
@@ -1,7 +1,7 @@
 package net.cryptic.app;
 
+import android.content.Context;
 import android.content.Intent;
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
